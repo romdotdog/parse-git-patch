575af7a8e8471112493f1265fc65f527ccb9b42b
ro-m-an <70765447+ro-m-an@users.noreply.github.com>
2021-02-17 11:07:26 -0600

    renamed files can have diffs

--- index.js
+++ index.js
@@ -1,125 +1,126 @@
-const hashRegex = /^From (\S*)/
-const authorRegex = /^From:\s?([^<].*[^>])?\s+(<(.*)>)?/
-const fileNameRegex = /^diff --git "?a\/(.*)"?\s*"?b\/(.*)"?/
-const fileLinesRegex = /^@@ -([0-9]*),?\S* \+([0-9]*),?/
-const similarityIndexRegex = /^similarity index /
-const addedFileModeRegex = /^new file mode /
-const deletedFileModeRegex = /^deleted file mode /
+const hashRegex = /^From (\S*)/;
+const authorRegex = /^From:\s?([^<].*[^>])?\s+(<(.*)>)?/;
+const fileNameRegex = /^diff --git "?a\/(.*)"?\s*"?b\/(.*)"?/;
+const fileLinesRegex = /^@@ -([0-9]*),?\S* \+([0-9]*),?/;
+const similarityIndexRegex = /^similarity index /;
+const addedFileModeRegex = /^new file mode /;
+const deletedFileModeRegex = /^deleted file mode /;
 
 function parseGitPatch(patch) {
-  if (typeof patch !== 'string') {
-    throw new Error('Expected first argument (patch) to be a string')
-  }
+	if (typeof patch !== "string") {
+		throw new Error("Expected first argument (patch) to be a string");
+	}
 
-  const lines = patch.split('\n')
+	const lines = patch.split("\n");
 
-  const hashLine = lines.shift()
-  const [, hash] = hashLine.match(hashRegex)
+	const hashLine = lines.shift();
+	const [, hash] = hashLine.match(hashRegex);
 
-  const authorLine = lines.shift()
-  const [, authorName,, authorEmail] = authorLine.match(authorRegex)
+	const authorLine = lines.shift();
+	const [, authorName, , authorEmail] = authorLine.match(authorRegex);
 
-  const dateLine = lines.shift()
-  const [, date] = dateLine.split('Date: ')
+	const dateLine = lines.shift();
+	const [, date] = dateLine.split("Date: ");
 
-  const messageLine = lines.shift()
-  const [, message] = messageLine.split('Subject: ')
+	const messageLine = lines.shift();
+	const [, message] = messageLine.split("Subject: ");
 
-  const parsedPatch = {
-    hash,
-    authorName,
-    authorEmail,
-    date,
-    message,
-    files: [],
-  }
+	const parsedPatch = {
+		hash,
+		authorName,
+		authorEmail,
+		date,
+		message,
+		files: []
+	};
 
-  splitIntoParts(lines, 'diff --git').forEach(diff => {
-    const fileNameLine = diff.shift()
-    const [, a, b] = fileNameLine.match(fileNameRegex)
-    const metaLine = diff.shift()
+	splitIntoParts(lines, "diff --git").forEach((diff) => {
+		const fileNameLine = diff.shift();
+		const [, a, b] = fileNameLine.match(fileNameRegex);
+		const metaLine = diff.shift();
 
-    const fileData = {
-      added: false,
-      deleted: false,
-      beforeName: a.trim(),
-      afterName: b.trim(),
-      modifiedLines: [],
-    }
+		const fileData = {
+			added: false,
+			deleted: false,
+			beforeName: a.trim(),
+			afterName: b.trim(),
+			modifiedLines: []
+		};
 
-    parsedPatch.files.push(fileData)
+		parsedPatch.files.push(fileData);
 
-    if (addedFileModeRegex.test(metaLine)) {
-      fileData.added = true
-    }
-    if (deletedFileModeRegex.test(metaLine)) {
-      fileData.deleted = true
-    }
-    if (similarityIndexRegex.test(metaLine)) {
-      return
-    }
+		if (addedFileModeRegex.test(metaLine)) {
+			fileData.added = true;
+		}
+		if (deletedFileModeRegex.test(metaLine)) {
+			fileData.deleted = true;
+		}
 
-    splitIntoParts(diff, '@@ ').forEach(lines => {
-      const fileLinesLine = lines.shift()
-      const [, a, b] = fileLinesLine.match(fileLinesRegex)
+		/*
+		if (similarityIndexRegex.test(metaLine)) {
+			return
+		}
+        */
 
-      let nA = parseInt(a)
-      let nB = parseInt(b)
+		splitIntoParts(diff, "@@ ").forEach((lines) => {
+			const fileLinesLine = lines.shift();
+			const [, a, b] = fileLinesLine.match(fileLinesRegex);
 
-      lines.forEach(line => {
-        nA++
-        nB++
+			let nA = parseInt(a);
+			let nB = parseInt(b);
 
-        if (line.startsWith('-- ')) {
-          return
-        }
-        if (line.startsWith('+')) {
-          nA--
+			lines.forEach((line) => {
+				nA++;
+				nB++;
 
-          fileData.modifiedLines.push({
-            added: true,
-            lineNumber: nB,
-            line: line.substr(1),
-          })
-        }
-        else if (line.startsWith('-')) {
-          nB--
+				if (line.startsWith("-- ")) {
+					return;
+				}
+				if (line.startsWith("+")) {
+					nA--;
 
-          fileData.modifiedLines.push({
-            added: false,
-            lineNumber: nA,
-            line: line.substr(1),
-          })
-        }
-      })
-    })
-  })
+					fileData.modifiedLines.push({
+						added: true,
+						lineNumber: nB,
+						line: line.substr(1)
+					});
+				} else if (line.startsWith("-")) {
+					nB--;
 
-  return parsedPatch
+					fileData.modifiedLines.push({
+						added: false,
+						lineNumber: nA,
+						line: line.substr(1)
+					});
+				}
+			});
+		});
+	});
+
+	return parsedPatch;
 }
 
 function splitIntoParts(lines, separator) {
-  const parts = []
-  let currentPart
+	const parts = [];
+	let currentPart;
 
-  lines.forEach(line => {
-    if (line.startsWith(separator)) {
-      if (currentPart) {
-        parts.push(currentPart)
-      }
+	lines.forEach((line) => {
+		if (line.startsWith(separator)) {
+			if (currentPart) {
+				parts.push(currentPart);
+			}
 
-      currentPart = [line]
-    }
-    else if (currentPart) {
-      currentPart.push(line)
-    }
-  })
+			currentPart = [line];
+		} else if (currentPart) {
+			currentPart.push(line);
+		}
+	});
 
-  if (currentPart) {
-    parts.push(currentPart)
-  }
+	if (currentPart) {
+		parts.push(currentPart);
+	}
 
-  return parts
+	return parts;
 }
 
-module.exports = parseGitPatch
+module.exports = parseGitPatch;
